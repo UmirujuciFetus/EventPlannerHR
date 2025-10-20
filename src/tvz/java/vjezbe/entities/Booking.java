5bf837eb950173d5145895378c0de4fba429c92b
UmirujuciFetus <uskokarlo@gmail.com>
2025-10-20 15:40:44 +0200

    dodane metode min i max i search, OBAVEZNO REFAKTORIRAT U ZASEBNE KLASE I SMANJIT MAIN TE DODAT TOSTRING"!!!!!!

--- src/tvz/java/vjezbe/entities/Booking.java
+++ src/tvz/java/vjezbe/entities/Booking.java
@@ -1,19 +1,20 @@
 package tvz.java.vjezbe.entities;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 import static tvz.java.vjezbe.app.Main.NUMBER_OF_USERS;
 
 public class Booking {
     private User user;
     private Ticket tickets;
+    private Integer bookingID;
 
 
-
-    public Booking(User user, Ticket tickets) {
+    public Booking(User user, Ticket tickets, Integer bookingID) {
         this.user = user;
         this.tickets = tickets;
-
+        this.bookingID = bookingID;
 
     }
 
@@ -31,17 +32,25 @@ public class Booking {
         return tickets;
     }
 
-    public static BigDecimal totalBookingPrice(){
-        Booking[] bookings = new Booking[NUMBER_OF_USERS];
-        BigDecimal sum = BigDecimal.ZERO;
-        for (Booking elem : bookings) {
-            sum = sum.add(elem.getTickets().getPrice());
-
-        }
-        return sum;
+    public void setBookingID(Integer bookingID){
+        this.bookingID = bookingID;
+    }
+    public Integer getBookingID(){
+        return bookingID;
     }
 
 
+    public static Booking bookingSearch(Booking[] bookings, Scanner sc){
+        Integer searchID = sc.nextInt();
+
+        for(Integer i =0;i<bookings.length;i++){
+            if(bookings[i].getBookingID().equals(searchID)){
+                return bookings[i];
+            }
+        }
+        System.out.println("Neispravan Booking ID!");
+        return null;
+    }
 
 }
 
